6e9addce7ec2b191b520173a48d26046f7ec6afe
Rohan Jacob <rohan.rj120@gmail.com>
2022-12-06 11:48:00 +0530

    Electronics Task 2

---
+++ Silicon-Tongue/Task2/AutoZ Task 2A.ino
@@ -0,0 +1,42 @@
+// Scaling Factor
+const float scale = 0.249266862;
+
+// Potentiometer reading
+int potVal;
+
+// Motor connections
+int e12 = 9;
+int i1 = 8;
+int i2 = 7;
+
+// LED connections
+int e34 = 3;
+int i3 = 5;
+int i4 = 4;
+
+
+void setup() {
+	// Declare control pins as outputs
+	pinMode(e12, OUTPUT);
+	pinMode(i1, OUTPUT);
+	pinMode(i2, OUTPUT);
+	pinMode(e34, OUTPUT);
+	pinMode(i3, OUTPUT);
+	pinMode(i4, OUTPUT);
+  	
+	// Set combinations required to move motor and turn on LED
+	digitalWrite(i1, HIGH);
+	digitalWrite(i2, LOW);
+	digitalWrite(i3, HIGH);
+	digitalWrite(i4, LOW);
+}
+
+
+void loop() {
+  	// Read potentiometer input
+  	potVal = analogRead(A5);
+    
+    // Write using PWM after scaling value down to 255 
+    analogWrite(e12, potVal*scale);
+    analogWrite(e34, potVal*scale);
+}
